e79edbffa8b5bcc7caa4eff38d53a234c53358c5
meixuguang <meixuguang@baidu.com>
2016-12-25 21:01:13 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: {
+    index: './js/index.js' // 入口文件
+  },
+  output: {
+    path: 'output',
+    filename: '[name].js' // 打包输出的文件
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/, // test 去判断是否为.js,是的话就是进行es6和jsx的编译
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/,
+        // query: {
+        //   presets: ['es2015', 'react']
+        // }
+      },
+      {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'}, // 用!去链式调用loader
+      {test: /\.css$/, loader: 'style-loader!css-loader'},
+      {test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'} // 内联的base64的图片地址，图片要小于8k，直接的url的地址则不解析
+    ]
+  }
+};
